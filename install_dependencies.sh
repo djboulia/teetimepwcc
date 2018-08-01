80dc056b7351195103001b90af6e9633922e7eb2
Don Boulia <djboulia@gmail.com>
2018-08-01 13:04:12 -0400

    make dependencies directory relative to main project

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,11 +1,11 @@
 #
 # install the two dependency jar files from the other Eclipse projects
 #
-mvn install:install-file -Dfile=/Users/djboulia/Development/workspace_teetime//webscraper/target/webscraper-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=webscraper -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=/Users/djboulia/Development/workspace_teetime/webscraper/pom.xml -DlocalRepositoryPath=./.m2/repository/
+mvn install:install-file -Dfile=../teetime/webscraper/target/webscraper-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=webscraper -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=../teetime/webscraper/pom.xml -DlocalRepositoryPath=./.m2/repository/
 
-mvn install:install-file -Dfile=/Users/djboulia/Development/workspace_teetime/teetime/target/teetime-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=teetime -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DlocalRepositoryPath=./.m2/repository
+mvn install:install-file -Dfile=../teetime/teetime/target/teetime-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=teetime -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DlocalRepositoryPath=./.m2/repository
 
-mvn install:install-file -Dfile=/Users/djboulia/Development/workspace_teetime//webscraper/target/webscraper-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=webscraper -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=/Users/djboulia/Development/workspace_teetime/webscraper/pom.xml
+mvn install:install-file -Dfile=../teetime/webscraper/target/webscraper-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=webscraper -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar -DpomFile=../teetime/webscraper/pom.xml
 
-mvn install:install-file -Dfile=/Users/djboulia/Development/workspace_teetime/teetime/target/teetime-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=teetime -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=../teetime/teetime/target/teetime-0.0.1-SNAPSHOT.jar -DgroupId=djboulia-golf -DartifactId=teetime -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
 
